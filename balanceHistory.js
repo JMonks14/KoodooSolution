1893910c608a874b7ef18a2758465791066979a7
James Monks <James.Monks@academytrainee.com>
2021-02-04 15:08:53 +0000

    solution written

---
+++ balanceHistory.js
@@ -0,0 +1,58 @@
+const accountBalanceHistory = [
+    {
+      monthNumber: 0, // current month
+      account: {
+        balance: { amount: 0 },
+      },
+    },
+    {
+      monthNumber: 1, // last month
+      account: {
+        balance: { amount: 100 },
+      },
+    },
+    {
+      monthNumber: 2, // two months ago
+      account: {
+        balance: { amount: 100 },
+      },
+    },
+    {
+        monthNumber: 3, // two months ago
+        account: {
+          balance: { amount: 100 },
+        },
+      }
+  ]
+
+  const accountTypeChecker = (accountBalanceHistory) => {
+    /***
+    Your goal is to write a function that determines from someone's ${accountBalanceHistory} 🧾 (see the array above for an example)
+    whether this array is of type A (variable) or type B (fixed).
+  
+    Type 🅰 denotes a balance history where the balance amount decreases by varying amounts each month.
+  
+    Type 🅱 is one where the balance amount changes by the same amount each month.
+    ***/
+  
+    // Write your logic here  - No pressure 😁 //
+    const months = accountBalanceHistory.length
+    if (months < 3) {
+        return "Not enough data"
+    }
+
+    let currentBalance = accountBalanceHistory[0].account.balance.amount
+    let initDif = accountBalanceHistory[1].account.balance.amount - currentBalance
+    
+    for (let i = 2; i < months; i++) {
+
+        let diff = accountBalanceHistory[i].account.balance.amount - accountBalanceHistory[i-1].account.balance.amount
+        if (diff != initDif) {
+            return "A"
+        }
+    }
+  
+    return "B";
+  };
+
+  console.log(accountTypeChecker(accountBalanceHistory))
